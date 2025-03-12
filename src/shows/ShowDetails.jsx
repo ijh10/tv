5a79356566ff8b54283ecb9e35495c98aa1b5c65
“IzzyHunter” <izzy@sensi.fit>
2025-03-12 09:53:52 -0400

    finished tv

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,21 @@
-import "./shows.css";
+import { useState } from "react";
+import{tvShows} from "./shows/data.js";
+import ShowDetails from "./show/ShowDetails.jsx";
+import ShowSelection from "./shows/ShowSelection.jsx";
 
-/** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
-}
+export default function App() {
+  const [selectedShow, setSelectedShow] = useState();
+  const [shows] = useState {tvShows}
+
+
+
+    return {
+      <header>
+        <><h1>React Tv</h1><ShowSelection shows={shows} setSelectedShow={setSelectedShow} /></>
+        </header>
+      <main>
+        <ShowDetails show={selectedShow}
+        </main>
+        </>
+    };
+  }
